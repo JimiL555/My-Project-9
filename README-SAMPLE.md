9bc7d82f43eeb39325d4bccfb01d6d10dd97afce
Jimi <liapis.jsl@gmail.com>
2024-08-05 16:19:41 -0400

    end-ish

---
+++ README-SAMPLE.md
@@ -0,0 +1,30 @@
+# ReadMe Generator
+
+## Description
+Creates ReadMe Files
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+See ReadMe File
+
+## Usage
+See ReadMe File
+
+## License
+This project is licensed under the Apache 2.0 license.
+
+## Contributing
+See ReadMe File
+
+## Tests
+Run node index.js
+
+## Questions
+If you have any questions, you can find me at [JimiL555](https://github.com/JimiL555) or reach out via email at Email@gmail.com.
--- index.js
+++ index.js
@@ -1,13 +1,109 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('README.md file has been generated!');
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, you can find me at [${data.github}](https://github.com/${data.github}) or reach out via email at ${data.email}.
+`;
+}
+
+// Function to initialize app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    const readmeContent = generateMarkdown(responses);
+    writeToFile('README.md', readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
